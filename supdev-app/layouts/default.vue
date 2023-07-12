f36f8aa436c55f8a4f1a38553b61f3f24766cb4e
Rakshit <rakshit087@gmail.com>
2023-07-12 16:57:40 +0530

    aligned login page to center

--- supdev-app/layouts/default.vue
+++ supdev-app/layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
-    <div class="min-h-screen transition-colors md:h-screen dark:bg-gray-900 dark:text-white">
+    <div class="transition-all h-screen dark:bg-gray-900 dark:text-white">
         <Navbar />
-        <div class="px-4 lg:px-32 xl:px-48">
+        <div class="px-4 lg:px-32 xl:px-48 flex flex-col justify-center h-full">
             <slot />
         </div>
     </div>
--- supdev-app/pages/login.vue
+++ supdev-app/pages/login.vue
@@ -9,10 +9,11 @@ definePageMeta({
 
 <template>
   <main>
-    <h1 className="mb-8 font-mono md:mb-16 text-7xl">SupDev &#60;3</h1>
-    <p className="font-mono leading-loose">
+    <h1 class="mb-8 font-mono md:mb-16  md:text-6xl text-5xl lg:text-7xl">SupDev &#60;3</h1>
+    <p class="font-mono leading-loose ">
       Verify your Github account on SupDev and mint your Github profile as an NFT to recieve support from the
       community for your open source contributions.
     </p>
+    <button class="mt-16 btn">Login with Github</button>
   </main>
 </template>
