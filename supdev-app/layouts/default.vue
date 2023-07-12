c2227ba49e1fcb00d7a1579aa790ccae6d2179cd
Rakshit <rakshit087@gmail.com>
2023-07-12 19:48:53 +0530

    finalsed login page

--- supdev-app/layouts/default.vue
+++ supdev-app/layouts/default.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="transition-all h-screen dark:bg-gray-900 dark:text-white">
+    <div class="transition-colors h-screen dark:bg-gray-900 dark:text-white">
         <Navbar />
         <div class="px-4 lg:px-32 xl:px-48 md:px-16 flex flex-col justify-center h-full">
             <slot />
