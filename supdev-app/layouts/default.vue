dadbadbfcf504107cbfe406ae7299c607187bf31
Rakshit <rakshit087@gmail.com>
2023-07-12 17:08:30 +0530

    added image in login page

--- supdev-app/layouts/default.vue
+++ supdev-app/layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="transition-all h-screen dark:bg-gray-900 dark:text-white">
         <Navbar />
-        <div class="px-4 lg:px-32 xl:px-48 flex flex-col justify-center h-full">
+        <div class="px-4 lg:px-32 xl:px-48 md:px-16 flex flex-col justify-center h-full">
             <slot />
         </div>
     </div>
--- supdev-app/pages/login.vue
+++ supdev-app/pages/login.vue
@@ -8,12 +8,15 @@ definePageMeta({
 </script>
 
 <template>
-  <main>
+  <main class="flex flex-col-reverse lg:flex-row justify-center items-center">
+    <div class="mr-0 lg:mr-8 text-center lg:text-left mt-12 md:mt-16 lg:mt-0">
     <h1 class="mb-8 font-mono md:mb-16  md:text-6xl text-5xl lg:text-7xl">SupDev &#60;3</h1>
     <p class="font-mono leading-loose ">
       Verify your Github account on SupDev and mint your Github profile as an NFT to recieve support from the
       community for your open source contributions.
     </p>
     <button class="mt-16 btn">Login with Github</button>
+  </div> 
+  <img src="~/assets/images/mint_nft.svg" alt="hero" class="w-52 md:w-96 lg:w-2/5">
   </main>
 </template>
